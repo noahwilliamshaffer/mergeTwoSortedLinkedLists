c1c6ec428f75955d1169872dd27a67f881bcb79c
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2020-08-06 17:41:43 -0400

    Update mergeTwoSortedLinkedLists.cpp

--- mergeTwoSortedLinkedLists.cpp
+++ mergeTwoSortedLinkedLists.cpp
@@ -1,8 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         //create a temporary linked list
         ListNode dummy(INT_MIN);
+        //create a pointer 
         ListNode *tail = &dummy;
         
         while (l1 && l2) {
@@ -20,3 +32,19 @@ public:
         return dummy.next;
     }
 };
+
+class Solution {
+public:
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+        while(l1 && l2) {
+            ListNode *& node = l1->val < l2->val ? l1 : l2;
+            tail = tail->next = node;
+            node = node->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+    
+   };
